910efb1bddde8e95d217493ed94818ad54dfc90e
IkennaGary <67379133+IkennaGary@users.noreply.github.com>
2024-08-06 11:16:19 +0100

    font and theme template setup initialized

---
+++ frontend/src/layouts/DashboardLayout.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const dashboardLayout = () => {
+  return <div>dashboardLayout</div>;
+};
+
+export default dashboardLayout;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#22331D",
+          dark: "#22331D",
+          light: "#6A6D69",
+        },
+        secondary: "#F65A11",
+        background: "#EFEDE7",
+      },
+      fontFamily: {
+        body: ["Switzer"],
+        heading: ["SwitzerExtraBold"],
+        subHeading: ["SwitzerBold"],
+        subHeading2: ["SwitzerNormal"],
+      },
+    },
   },
   plugins: [],
 };
